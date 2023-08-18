5058e33f5ac1f4c7e2086c11c7ab72d53bc56c40
qzhang <qzhang@apac.freewheel.com>
2023-08-18 12:08:34 +0800

    update slack channel

--- cmd/root.go
+++ cmd/root.go
@@ -11,8 +11,8 @@ var (
 		Short: "A tool to make Pull Requests and Cherry-picking easy.",
 		Long: `fwpr is a tool to create multiple Pull Requests based on current branch to make Cherry-picking easy.
 
-Get more details from https://github.freewheel.tv/qzhang/fwpr. 
-Join our slack channel to submit issues and get supported #kof-user-group https://freewheel.slack.com/archives/C05M8MQNMQQ
+Get more details from https://github.freewheel.tv/qzhang/fwpr
+Join our slack channel to submit issues and get supported #fw-useful-tools https://freewheel.slack.com/archives/C05NA6TPM2R
 `,
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			if debug {
